c78706bb8706117a280cb0d5729a3e1c1140f6ff
Marton Magyar <mr.tilk@gmail.com>
2019-07-05 23:50:36 +0200

    version 2.0

---
+++ script.js
@@ -0,0 +1,7 @@
+let navImg = document.getElementById("nav-img");
+console.log(navImg.parentElement);
+
+let searchImg = document.getElementById("search-bar-img");
+console.log(searchImg.parentElement);
+
+document.getElementById("search-bar").focus();
